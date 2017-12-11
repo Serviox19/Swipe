dd51b0aff14103486f2a201ac7c3987a4894ab17
Servio Mora <servio.mora19@gmail.com>
2017-12-10 19:37:44 -0500

    successfully moving cards with panResponder

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -12,35 +12,41 @@ export default class Deck extends Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
+
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
         console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {}
-    });
+      onPanResponderRelease: () => {
 
-    this.state = { panResponder };
+      }
+    });
+    this.state = { panResponder, position };
   }
 
   renderCards() {
     return this.props.data.map(card => {
       return (
-        <Card
-          {...this.state.panResponder.panHandlers}
-          image={{ uri: card.poster }}
-          imageStyle={{ height: 300 }}
-          title={card.title}
-          key={card.release_date}
-        >
-          <Text style={{ marginBottom: 15 }}>{moment(card.release_date).format("MMMM DD YYYY")}</Text>
-          <Text style={{ marginBottom: 15 }}>{card.description}</Text>
-          <Button
-            icon={{ name: 'code' }}
-            backgroundColor="#03A9F4"
-            title="View Now"
-          />
-        </Card>
+        <Animated.View style={this.state.position.getLayout()}>
+          <Card
+            {...this.state.panResponder.panHandlers}
+            image={{ uri: card.poster }}
+            imageStyle={{ height: 300 }}
+            title={card.title}
+            key={card.release_date}
+          >
+            <Text style={{ marginBottom: 15 }}>{moment(card.release_date).format("MMMM DD YYYY")}</Text>
+            <Text style={{ marginBottom: 15 }}>{card.description}</Text>
+            <Button
+              icon={{ name: 'code' }}
+              backgroundColor="#03A9F4"
+              title="View Now"
+            />
+          </Card>
+        </Animated.View>
       );
     });
   }
