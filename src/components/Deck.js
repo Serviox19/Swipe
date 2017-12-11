5f1559536a7f10224e27b61f0097ee9e33197ea9
Servio Mora <servio.mora19@gmail.com>
2017-12-10 20:32:38 -0500

    spring it back

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -3,10 +3,13 @@ import {
   View,
   Text,
   Animated,
-  PanResponder
+  PanResponder,
+  Dimensions
 } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 import moment from 'moment';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class Deck extends Component {
   constructor(props) {
@@ -18,25 +21,63 @@ export default class Deck extends Component {
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
         console.log(gesture);
-        position.setValue({ x: gesture.dx, y: gesture.dy });
+        position.setValue({ x: gesture.dx });
       },
       onPanResponderRelease: () => {
-
+        this.resetPosition();
       }
     });
     this.state = { panResponder, position };
   }
 
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: {x: 0, y: 0}
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+
   renderCards() {
-    return this.props.data.map(card => {
+    return this.props.data.map((card, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View key={0} style={this.getCardStyle()}>
+            <Card
+              {...this.state.panResponder.panHandlers}
+              image={{ uri: card.poster }}
+              imageStyle={{ height: 300 }}
+              title={card.title}
+              key={card.release_date}
+            >
+              <Text style={{ marginBottom: 15 }}>{moment(card.release_date).format("MMMM DD YYYY")}</Text>
+              <Text style={{ marginBottom: 15 }}>{card.description}</Text>
+              <Button
+                icon={{ name: 'code' }}
+                backgroundColor="#03A9F4"
+                title="View Now"
+              />
+            </Card>
+          </Animated.View>
+        )
+      }
       return (
-        <Animated.View style={this.state.position.getLayout()}>
+        <View key={card.release_date}>
           <Card
-            {...this.state.panResponder.panHandlers}
             image={{ uri: card.poster }}
             imageStyle={{ height: 300 }}
             title={card.title}
-            key={card.release_date}
           >
             <Text style={{ marginBottom: 15 }}>{moment(card.release_date).format("MMMM DD YYYY")}</Text>
             <Text style={{ marginBottom: 15 }}>{card.description}</Text>
@@ -46,7 +87,7 @@ export default class Deck extends Component {
               title="View Now"
             />
           </Card>
-        </Animated.View>
+        </View>
       );
     });
   }
